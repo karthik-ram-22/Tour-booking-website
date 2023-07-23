933b55d95cc8de9ac9919b5abf8a9860d6aa2421
karthik-ram-22 <114208155+karthik-ram-22@users.noreply.github.com>
2023-07-23 22:10:20 +0530

    Delete bookingform.js

--- bookingform.js
+++
@@ -1,8 +0,0 @@
-function validate() {
-    let x = document.getElementsByID("mem").value;
-
-    if (isNaN(x) || x < 1) {
-        window.alert("Enter a valid number");
-    }
-    windoow.location.href="payment.html";
-}
